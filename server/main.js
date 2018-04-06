7606038e95e842308c9433c0de48ca5c28da09d4
yeremiakpan <yeremiakpan@gmail.com>
2018-04-06 09:04:18 +0400

    Doctors can reply

--- server/main.js
+++ server/main.js
@@ -3,3 +3,8 @@ import { Meteor } from 'meteor/meteor';
 Meteor.startup(() => {
   // code to run on server at startup
 });
+
+Meteor.publish('users', function () {
+    Meteor.users.find({roles :{ $in : ['doctor']}});
+});
+
