8c5439052f56fc5a9872876d51fa1d805f536594
yeremiakpan <yeremiakpan@gmail.com>
2018-04-08 20:21:38 +0400

    subscibed to user profile

--- client/templates/doctors/doctors.js
+++ client/templates/doctors/doctors.js
@@ -2,6 +2,10 @@ Template.doctors.onCreated(function () {
    this.subscribe('doctorUsers');
 });
 
+Template.doctors.onCreated(function () {
+    this.subscribe('doctors');
+});
+
 Template.doctors.helpers({
    users: function(){
        return Meteor.users.find({ username: {$not: (Meteor.user() || {}).username }}); //Search through the returned list of doctors and ensure current user is not listed.
