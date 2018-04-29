fe7e0545cab675331a4e0d87f47aa400a3ea8116
yeremiakpan <yeremiakpan@gmail.com>
2018-04-29 17:59:37 +0400

    Comments Added to Consultation Section

--- client/templates/consult/consult.js
+++ client/templates/consult/consult.js
@@ -6,16 +6,26 @@ Template.consult.onCreated(function (){
 //Provide the consult template with database objects needed
 Template.consult.helpers({
    availableConsultations: function () {
+
+       //Get the currently logged in user
        var user = Meteor.user();
+
+       //Get the list of patient's favourite doctors or empty array if they have no favourite doctor
        var favouriteDoctors = user.profile.favouriteDoctors || [];
 
+       //Find all ongoing consultations
        Consultations.find({ status: null}).forEach(function (consultations) {
+
+           //Set the identity of the participants, 'd' for doctor and 'p' for patient
            var identity = (consultations.p === user._id) ? 'd': 'p';
            var idx = favouriteDoctors.indexOf((consultations[identity]));
 
+           //Remove the existing consultation from the array
            if (idx > -1) favouriteDoctors.splice(idx, 1);
        });
         console.log('favouriteDoctors');
+
+        //Return the st of doctors we can consult
        return favouriteDoctors.length ? Meteor.users.find({ _id: { $in: favouriteDoctors } } ) : null;
    }
 });
--- server/publications.js
+++ server/publications.js
@@ -5,7 +5,7 @@ Meteor.publish('users', function(){
     return Meteor.users.find();
 });
 
-//Publish list of consultations to the client
+//Publish list of consultations where the current user is either the doctor or the patient
 Meteor.publish('consultations', function(){
     this.userId;
     return Consultations.find({  $or: [{ p: this.userId }, { d: this.userId}]});
